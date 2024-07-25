0c809d488e9b3fa7b160ae57852c3956c5ac54fe
Savindi20 <savindidadallage@gmail.com>
2024-07-25 10:25:36 +0530

    Search Box added

--- app/screens/HomeScreen.jsx
+++ app/screens/HomeScreen.jsx
@@ -1,11 +1,27 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState } from "react";
 
-import { MaterialIcons } from '@expo/vector-icons'
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
 import { Screen3 } from "../assets";
 
-
 const HomeScreen = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (text) => {
+    setSearchTerm(text);
+
+    setFiltered(feeds?.feeds.filter((item) => item.title.includes(text)));
+  };
+
   return (
     <SafeAreaView className="flex-1 items-center justify-Start bg-[#EBEA]">
       <View className="w-full flex-row items-center justify-between px-4 py-2">
@@ -16,6 +32,25 @@ const HomeScreen = () => {
           resizeMode="cover"
         />
       </View>
+
+      {/* Search Box starts here */}
+      <View className="flex-row items-center justify-between px-4 py-2 w-full space-x-6">
+        <View className="px-4 py-2 bg-white rounded-xl flex-1 flex-row items-center justify-center space-x-2">
+          <MaterialIcons name="search" size={24} color="#7f7f7f" />
+          <TextInput
+            className="text-base font-semibold text=[#555] flex-1 py-1 -mt-1 "
+            placeholder="Search Here..."
+            value={searchTerm}
+            onChangeText={handleSearchTerm}
+          />
+        </View>
+
+        <TouchableOpacity className="w-12 h-12 rounded-xl flex items-center justify-center bg-white">
+          <FontAwesome name="filter" size={24} color="#7f7f7f" />
+        </TouchableOpacity>
+      </View>
+      {/* search box ends here */}
+
     </SafeAreaView>
   )
 }
